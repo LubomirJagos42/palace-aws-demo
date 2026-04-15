f3e59748622eb509449dd941e912e5005cde2fe8
Lubomir Jagos <lubomir.jagos@gmail.com>
2026-04-15 16:15:25 +0200

    Remove utils (will be submodule)

--- utils/src/basicpalacesolverhelperpackage/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-BasicMfemMesher - Mesh creation utilities for Palace solver.
-"""
-
-from .BasicMfemMesher import BasicMfemMesher
-from .PlotDiagramUtils import PlotDiagramUtils
-
-__version__ = "0.1.0"
-__all__ = ["BasicMfemMesher", "PlotDiagramUtils"]
